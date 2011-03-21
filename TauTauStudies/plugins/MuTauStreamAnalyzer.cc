c3e7db35fecf8598cb8c7e954b1bfaa52cd46fbf
bianchi <>
2011-03-21 17:45:21 +0000

    added analyzer for Mu+Tau stream

---
+++ TauTauStudies/plugins/MuTauStreamAnalyzer.cc
@@ -0,0 +1,362 @@
+#include "Bianchi/TauTauStudies/interface/MuTauStreamAnalyzer.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+
+#include "TauAnalysis/CandidateTools/interface/CompositePtrCandidateT1T2MEtProducer.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEtFwd.h"
+
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+
+#include "DataFormats/GeometryVector/interface/VectorUtil.h"
+
+#include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
+#include "DataFormats/RecoCandidate/interface/IsoDepositFwd.h"
+#include "DataFormats/RecoCandidate/interface/IsoDepositDirection.h"
+#include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
+
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include <vector>
+#include <utility>
+#include <map>
+
+using namespace std;
+using namespace reco;
+
+typedef std::map<double, math::XYZTLorentzVectorD ,MuTauStreamAnalyzer::more>::iterator CImap;
+
+MuTauStreamAnalyzer::MuTauStreamAnalyzer(const edm::ParameterSet & iConfig){
+
+  diTauTag_ = iConfig.getParameter<edm::InputTag>("diTaus");
+  jetsTag_ = iConfig.getParameter<edm::InputTag>("jets");
+  isMC_ =  iConfig.getParameter<bool>("isMC");
+  minCorrPt_  =  iConfig.getUntrackedParameter<double>("minCorrPt",10.);
+  minJetID_   =  iConfig.getUntrackedParameter<double>("minJetID",0.5);
+  verbose_ =  iConfig.getUntrackedParameter<bool>("verbose",false);
+}
+
+void MuTauStreamAnalyzer::beginJob(){
+
+  edm::Service<TFileService> fs;
+  tree_ = fs->make<TTree>("tree","qqH tree");
+
+  jetsBtagHE_  = new std::vector< double >();
+  jetsBtagHP_  = new std::vector< double >();
+
+  jetsP4_          = new std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >();
+  jetsIDP4_        = new std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >();
+
+  diTauVisP4_ = new std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >();
+  diTauCAP4_ = new std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >();
+  diTauSVfitP4_ = new std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >();
+
+
+  tree_->Branch("jetsP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",&jetsP4_);
+  tree_->Branch("jetsIDP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",&jetsIDP4_);
+  
+  tree_->Branch("jetsBtagHE","std::vector<double> ",&jetsBtagHE_);
+  tree_->Branch("jetsBtagHP","std::vector<double> ",&jetsBtagHP_);
+
+  tree_->Branch("diTauVisP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",  &diTauVisP4_);
+  tree_->Branch("diTauCAP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",   &diTauCAP4_);
+  tree_->Branch("diTauSVfitP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",&diTauSVfitP4_);
+
+  tree_->Branch("METP4","std::vector< ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >",&METP4_);
+  tree_->Branch("sumEt",&sumEt_,"sumEt/F");
+  tree_->Branch("MtLeg1",&MtLeg1_,"MtLeg1/F");
+
+  tree_->Branch("chIsoLeg1",&chIsoLeg1_,"chIsoLeg1/F");
+  tree_->Branch("nhIsoLeg1",&nhIsoLeg1_,"nhIsoLeg1/F");
+  tree_->Branch("phIsoLeg1",&phIsoLeg1_,"phIsoLeg1/F");
+  tree_->Branch("chIsoLeg2",&chIsoLeg2_,"chIsoLeg2/F");
+  tree_->Branch("nhIsoLeg2",&nhIsoLeg2_,"nhIsoLeg2/F");
+  tree_->Branch("phIsoLeg2",&phIsoLeg2_,"phIsoLeg2/F");
+  tree_->Branch("dxy1",&dxy1_,"dxy1/F");
+  tree_->Branch("dxy2",&dxy2_,"dxy2/F");
+
+  tree_->Branch("run",&run_,"run/F");
+  tree_->Branch("event",&event_,"event/F");
+  tree_->Branch("numPV",&numPV_,"numPV/F");
+  tree_->Branch("numOfDiTaus",&numOfDiTaus_,"numOfDiTaus/I");
+
+}
+
+
+MuTauStreamAnalyzer::~MuTauStreamAnalyzer(){
+  delete jetsP4_; delete METP4_; delete diTauVisP4_; delete diTauCAP4_; delete diTauSVfitP4_;
+}
+
+void MuTauStreamAnalyzer::analyze(const edm::Event & iEvent, const edm::EventSetup & iSetup){
+
+
+  jetsP4_->clear();
+  jetsIDP4_->clear();
+  diTauVisP4_->clear();
+  diTauCAP4_->clear();
+  diTauSVfitP4_->clear();
+  METP4_->clear();
+
+  
+  edm::Handle<PATMuTauPairCollection> diTauHandle;
+  iEvent.getByLabel(diTauTag_,diTauHandle);
+  if( !diTauHandle.isValid() )  
+    edm::LogError("DataNotAvailable")
+      << "No diTau label available \n";
+  const PATMuTauPairCollection* diTaus = diTauHandle.product();
+
+  edm::Handle<pat::JetCollection> jetsHandle;
+  iEvent.getByLabel(jetsTag_,jetsHandle);
+  if( !jetsHandle.isValid() )  
+    edm::LogError("DataNotAvailable")
+      << "No jets label available \n";
+  const pat::JetCollection* jets = jetsHandle.product();
+
+  edm::Handle<reco::VertexCollection> pvHandle;
+  edm::InputTag pvTag("offlinePrimaryVerticesWithBS");
+  iEvent.getByLabel(pvTag,pvHandle);
+  if( !pvHandle.isValid() )  
+    edm::LogError("DataNotAvailable")
+      << "No PV label available \n";
+  const reco::VertexCollection* vertexes = pvHandle.product();
+  numPV_ = vertexes->size();
+
+  edm::Handle<pat::METCollection> metHandle;
+  iEvent.getByLabel(edm::InputTag("patMETsPFlow"),metHandle);
+  if( !metHandle.isValid() )  
+    edm::LogError("DataNotAvailable")
+      << "No MET label available \n";
+  const pat::METCollection* met = metHandle.product();
+
+  
+  const PATMuTauPair *theDiTau = 0;
+  if(diTaus->size()<1){
+    cout << " No diTau !!! " << endl;
+    return;
+  } else if(diTaus->size()>1 && verbose_){
+    cout << "WARNING: "<< diTaus->size() << "  diTaus found in the event !!! " << endl;
+  }
+  theDiTau = &(*diTaus)[0];
+
+  numOfDiTaus_ = diTaus->size();
+  METP4_->push_back((*met)[0].p4());
+  sumEt_  = (*met)[0].sumEt();
+  MtLeg1_ =  theDiTau->mt1MET();
+
+  const pat::Muon* leg1 = dynamic_cast<const pat::Muon*>( (theDiTau->leg1()).get() );
+  const pat::Tau*  leg2 = dynamic_cast<const pat::Tau*>(  (theDiTau->leg2()).get() );
+
+  dxy1_ = vertexes->size()!=0 ? leg1->globalTrack()->dxy( (*vertexes)[0].position() ) : -999;
+  dxy2_ = (vertexes->size()!=0 && 
+	   (leg2->leadPFChargedHadrCand()).isNonnull() 
+	   && (leg2->leadPFChargedHadrCand()->trackRef()).isNonnull() ) ? 
+    leg2->leadPFChargedHadrCand()->trackRef()->dxy( (*vertexes)[0].position() ) : -999;
+
+  isodeposit::AbsVetos vetosChargedLeg1; 
+  isodeposit::AbsVetos vetosNeutralLeg1; 
+  isodeposit::AbsVetos vetosPhotonLeg1;
+ 
+  vetosChargedLeg1.push_back(new isodeposit::ThresholdVeto(0.5));
+  vetosNeutralLeg1.push_back(new isodeposit::ConeVeto(isodeposit::Direction(leg1->eta(),leg1->phi()),0.08));
+  vetosNeutralLeg1.push_back(new isodeposit::ThresholdVeto(1.0));
+  vetosPhotonLeg1.push_back(new isodeposit::ConeVeto(isodeposit::Direction(leg1->eta(),leg1->phi()),0.05));
+  vetosPhotonLeg1.push_back(new isodeposit::ThresholdVeto(1.0));
+
+  
+
+  chIsoLeg1_ = 
+    leg1->isoDeposit(pat::PfChargedHadronIso)->depositAndCountWithin(0.4,vetosChargedLeg1).first;
+  nhIsoLeg1_ = 
+    leg1->isoDeposit(pat::PfNeutralHadronIso)->depositAndCountWithin(0.4,vetosNeutralLeg1).first;
+  phIsoLeg1_ = 
+    leg1->isoDeposit(pat::PfGammaIso)->depositAndCountWithin(0.4,vetosPhotonLeg1).first;
+  chIsoLeg2_ = -99;
+  nhIsoLeg2_ = -99;
+  phIsoLeg2_ = -99;
+   
+
+  // cleaning
+  for(unsigned int i = 0; i <vetosChargedLeg1.size(); i++){
+    delete vetosChargedLeg1[i];
+  }
+  for(unsigned int i = 0; i <vetosNeutralLeg1.size(); i++){
+    delete vetosNeutralLeg1[i];
+    delete vetosPhotonLeg1[i];
+  }
+  //
+
+  diTauVisP4_->push_back( theDiTau->p4Vis() );
+  diTauCAP4_->push_back( theDiTau->p4ImprovedCollinearApprox() );
+  diTauSVfitP4_->push_back( theDiTau->svFitSolution("psKine_MEt_ptBalance","",0)->p4()  );
+
+  run_   = iEvent.run();
+  event_ = (iEvent.eventAuxiliary()).event();
+  
+  std::map<double, math::XYZTLorentzVectorD ,MuTauStreamAnalyzer::more> sortedJets;
+  std::map<double, math::XYZTLorentzVectorD ,MuTauStreamAnalyzer::more> sortedJetsID;
+
+  for(unsigned int it = 0; it < jets->size() ; it++){
+
+    if(verbose_){
+      pat::Jet* jet = const_cast<pat::Jet*>(&(*jets)[it]);
+      //for(unsigned int i = 0; i < (jet->availableJECLevels()).size() ; i++ ){
+      //std::cout << (jet->availableJECLevels())[i] << std::endl;
+      //}
+      std::cout << "Uncorrected " << jet->correctedJet("Uncorrected").pt() << std::endl;
+      std::cout << "L1FastJet "   << jet->correctedJet("L1FastJet").pt() << std::endl;
+      std::cout << "L2Relative "  << jet->correctedJet("L2Relative").pt() << std::endl; 
+      std::cout << "L3Absolute "  << jet->correctedJet("L3Absolute").pt() << std::endl; 
+    }
+
+    if( jetID( &(*jets)[it] ) < minJetID_ )  continue;
+
+    sortedJets.insert( make_pair( (*jets)[it].correctedJet("Uncorrected").p4().Pt() ,(*jets)[it].correctedJet("Uncorrected").p4() ) );
+
+    if((*jets)[it].p4().Pt() < minCorrPt_) continue;
+
+    //add b-tag info
+    jetsBtagHE_->push_back((*jets)[it].bDiscriminator("trackCountingHighEffBJetTags"));
+    jetsBtagHP_->push_back((*jets)[it].bDiscriminator("trackCountingHighPurBJetTags"));
+                                
+    sortedJetsID.insert( make_pair( (*jets)[it].p4().Pt() ,(*jets)[it].p4() ) );
+     
+  }
+  
+  for(CImap it = sortedJets.begin(); it != sortedJets.end() ; it++){
+    jetsP4_->push_back( it->second );
+  }
+  for(CImap it = sortedJetsID.begin(); it != sortedJetsID.end() ; it++){
+    jetsIDP4_->push_back( it->second );
+  }
+
+
+  tree_->Fill();
+
+}
+
+
+unsigned int MuTauStreamAnalyzer::jetID( const pat::Jet* jet){
+
+  if( (jet->pt())<10 ) return 99; // always pass jet ID
+
+  std::vector<reco::PFCandidatePtr> pfCandPtrs = jet->getPFConstituents();
+
+  int nCharged = 0;
+  int nPhotons = 0;
+  int nNeutral = 0;
+  int nConst = 0;
+
+  float energyCharged = 0;
+  float energyPhotons = 0;
+  float energyNeutral = 0;
+  float energyElectrons = 0;
+ 
+  float totalEnergyFromConst = 0;
+
+  for(unsigned i=0; i<pfCandPtrs.size(); ++i) {
+    const reco::PFCandidate& cand = *(pfCandPtrs[i]);
+
+    totalEnergyFromConst +=  cand.energy();
+    nConst += 1;
+
+    switch( cand.particleId() ) {
+    case reco::PFCandidate::h: 
+      nCharged++;
+      energyCharged += cand.energy(); 
+      break;
+    case reco::PFCandidate::gamma:
+      nPhotons++;
+      energyPhotons += cand.energy();
+      break;
+    case reco::PFCandidate::h0:
+      nNeutral++;
+      energyNeutral += cand.energy();
+      break;
+    case reco::PFCandidate::e: 
+      energyElectrons += cand.energy(); 
+      break;
+    case reco::PFCandidate::h_HF: // fill neutral
+      nNeutral++;
+      energyNeutral += cand.energy();
+      break;
+    case reco::PFCandidate::egamma_HF: // fill e/gamma
+      nPhotons++;
+      energyPhotons += cand.energy();
+      break;
+    default:
+      break;
+    }
+  }
+
+  bool loose=false;
+  bool medium=false;
+  bool tight=false;
+
+  //loose id
+  if( (TMath::Abs(jet->eta())>2.4 && 
+       energyNeutral/totalEnergyFromConst<0.99 && 
+       energyPhotons/totalEnergyFromConst<0.99 &&
+       nConst > 1) || 
+      (TMath::Abs(jet->eta())<2.4 && 
+       energyNeutral/totalEnergyFromConst<0.99 && 
+       energyPhotons/totalEnergyFromConst<0.99 &&
+       nConst > 1 &&
+       energyCharged/totalEnergyFromConst>0 &&
+       nCharged>0 &&
+       energyElectrons/totalEnergyFromConst<0.99
+       )
+      ) loose = true;
+  // medium id
+  if( (TMath::Abs(jet->eta())>2.4 && 
+       energyNeutral/totalEnergyFromConst<0.95 && 
+       energyPhotons/totalEnergyFromConst<0.95 &&
+       nConst > 1) || 
+      (TMath::Abs(jet->eta())<2.4 && 
+       energyNeutral/totalEnergyFromConst<1 && 
+       energyPhotons/totalEnergyFromConst<1 &&
+       nConst > 1 &&
+       energyCharged/totalEnergyFromConst>0 &&
+       nCharged>0 &&
+       energyElectrons/totalEnergyFromConst<1
+       )
+      ) medium = true;
+  // tight id
+  if( (TMath::Abs(jet->eta())>2.4 && 
+       energyNeutral/totalEnergyFromConst<0.90 && 
+       energyPhotons/totalEnergyFromConst<0.90 &&
+       nConst > 1) || 
+      (TMath::Abs(jet->eta())<2.4 && 
+       energyNeutral/totalEnergyFromConst<1 && 
+       energyPhotons/totalEnergyFromConst<1 &&
+       nConst > 1 &&
+       energyCharged/totalEnergyFromConst>0 &&
+       nCharged>0 &&
+       energyElectrons/totalEnergyFromConst<1
+       )
+      ) tight = true;
+  
+  if(loose && !medium && !tight) return 1;
+  if(loose && medium && !tight)  return 2;
+  if(loose && medium && tight)   return 3; 
+  
+  return 0;
+
+}
+
+
+
+void MuTauStreamAnalyzer::endJob(){}
+
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+ 
+DEFINE_FWK_MODULE(MuTauStreamAnalyzer);
+
+
---
+++ TauTauStudies/test/patTuple_PAT_MuTauStream_cfg.py
@@ -0,0 +1,358 @@
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.PyReleaseValidation.autoCond import autoCond
+process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
+
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+process.kt6PFJets.doRhoFastjet = True
+process.kt6PFJets.Rho_EtaMax = cms.double(4.4)
+process.kt6PFJets.Ghost_EtaMax = cms.double(5.0)
+process.ak5PFJets.doAreaFastjet = True
+process.ak5PFJets.Rho_EtaMax = cms.double(4.4)
+process.ak5PFJets.Ghost_EtaMax = cms.double(5.0)
+
+process.ak5PFL1Fastjet.useCondDB = False
+
+process.fjSequence = cms.Sequence(process.kt6PFJets+process.ak5PFJets)
+
+process.source.fileNames = cms.untracked.vstring(
+    #'file:/data_CMS/cms/lbianchini/ZTT_RelVal386_1.root',
+    #'file:/data_CMS/cms/lbianchini/ZMuMu_RelVal386.root',
+    'rfio:/dpm/in2p3.fr/home/cms/trivcat//store/mc/Fall10/VBF_HToTauTau_M-115_7TeV-powheg-pythia6-tauola/GEN-SIM-RECO/START38_V12-v1/0000/044E940A-55EC-DF11-89D6-0023AEFDEE60.root',
+    #'file:/data_CMS/cms/lbianchini/F41A3437-7AED-DF11-A50D-002618943894.root',
+    #'file:/data_CMS/cms/lbianchini/ZElEl_RelVal386_1.root',
+    )
+
+#process.source.eventsToProcess = cms.untracked.VEventRange(
+#    '1:1080','1:2003','1:2028','1:6867','1:7016'
+#    )
+
+postfix           = "PFlow"
+sample            = ""
+runOnMC           = True
+
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.printTree1 = cms.EDAnalyzer("ParticleListDrawer",
+                                    src = cms.InputTag("genParticles"),
+                                    maxEventsToPrint  = cms.untracked.int32(1)
+                                    )
+
+process.primaryVertexFilter = cms.EDFilter(
+    "GoodVertexFilter",
+    vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+    minimumNDOF = cms.uint32(4) ,
+    maxAbsZ = cms.double(24),
+    maxd0 = cms.double(2)
+    )
+
+process.scrapping = cms.EDFilter(
+    "FilterOutScraping",
+    applyfilter = cms.untracked.bool(True),
+    debugOn = cms.untracked.bool(False),
+    numtrack = cms.untracked.uint32(10),
+    thresh = cms.untracked.double(0.25)
+    )
+
+process.allEventsFilter = cms.EDFilter(
+    "AllEventsFilter"
+    )
+
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
+if not runOnMC:
+    removeMCMatching(process,["All"])
+    
+removeSpecificPATObjects(process, ['Photons'],
+                         outputInProcess=False)
+removeCleaning(process,
+               outputInProcess=False)
+
+restrictInputToAOD(process, ['All'])
+
+from Bianchi.Utilities.customizePAT  import *
+addSelectedPFlowParticle(process)
+
+from PhysicsTools.PatAlgos.tools.metTools import *
+addPfMET(process, postfix)
+
+from PhysicsTools.PatAlgos.tools.jetTools import *
+switchJetCollection(process,cms.InputTag('ak5PFJets'),
+                    doJTA        = True,
+                    doBTagging   = True,
+                    jetCorrLabel = ('AK5PF', ['L2Relative', 'L3Absolute',]),
+                    doType1MET   = False,
+                    genJetCollection=cms.InputTag("ak5GenJets"),
+                    doJetID      = True,
+                    jetIdLabel   = 'ak5'
+                    )
+
+JEClevels = cms.vstring(['L2Relative', 'L3Absolute'])
+if runOnMC:
+    JEClevels = ['L1FastJet', 'L2Relative', 'L3Absolute']
+else:
+    JEClevels = ['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']
+
+process.patJetCorrFactors.levels = JEClevels
+process.patJetCorrFactors.rho = cms.InputTag('kt6PFJets','rho')
+
+
+## <tau part>
+from PhysicsTools.PatAlgos.tools.tauTools import *
+switchToPFTauHPS(process, 
+                 pfTauLabelOld = 'shrinkingConePFTauProducer',
+                 pfTauLabelNew = 'hpsPFTauProducer'
+                 )
+
+getattr(process,"patTaus").embedIsolationTracks = cms.bool(True)
+getattr(process,"patTaus").embedSignalTracks = cms.bool(True)
+getattr(process,"patTaus").embedGenMatch = cms.bool(True)
+getattr(process,"patTaus").embedLeadTrack = cms.bool(True)
+getattr(process,"patTaus").embedLeadPFCand = True
+getattr(process,"patTaus").embedLeadPFChargedHadrCand = True
+getattr(process,"patTaus").embedLeadPFNeutralCand = True
+getattr(process,"patTaus").embedSignalPFCands = True
+getattr(process,"patTaus").embedSignalPFChargedHadrCands = True
+getattr(process,"patTaus").embedSignalPFNeutralHadrCands = True
+getattr(process,"patTaus").embedSignalPFGammaCands = True
+getattr(process,"patTaus").embedIsolationPFCands = True
+getattr(process,"patTaus").embedIsolationPFChargedHadrCands = True
+getattr(process,"patTaus").embedIsolationPFNeutralHadrCands = True
+getattr(process,"patTaus").embedIsolationPFGammaCands = True
+getattr(process,"patTaus").embedGenJetMatch = cms.bool(True)
+
+
+setattr(process,"hpsPFTauDiscriminationAgainstElectronCrackRem",
+        getattr(process,"hpsPFTauDiscriminationAgainstElectron").clone(
+    ApplyCut_EcalCrackCut = cms.bool(True),
+    ApplyCut_PFElectronMVA =  cms.bool(False)
+    )
+        )
+
+process.patHPSPFTauDiscrimination += process.hpsPFTauDiscriminationAgainstElectronCrackRem
+
+getattr(process,"makePatTaus").replace(
+    getattr(process,"patTaus"),
+    process.patHPSPFTauDiscrimination + getattr(process,"patTaus")
+    )
+
+getattr(process,"patTaus").tauIDSources = cms.PSet(
+    leadingTrackFinding = cms.InputTag("hpsPFTauDiscriminationByDecayModeFinding"),
+    byLooseIsolation = cms.InputTag("hpsPFTauDiscriminationByLooseIsolation"),
+    byMediumIsolation = cms.InputTag("hpsPFTauDiscriminationByMediumIsolation"),
+    byTightIsolation = cms.InputTag("hpsPFTauDiscriminationByTightIsolation"),
+    againstElectron = cms.InputTag("hpsPFTauDiscriminationAgainstElectron"),
+    againstElectronCrackRem = cms.InputTag("hpsPFTauDiscriminationAgainstElectronCrackRem"),
+    againstMuon = cms.InputTag("hpsPFTauDiscriminationAgainstMuon")
+    )
+
+## <\tau part>
+
+addPFMuonIsolation(process,process.patMuons)
+addPFMuon(process,postfix)
+addTriggerMatchingMuon(process)
+getattr(process,"patMuons").embedTrack = True
+getattr(process,"patMuons"+postfix).embedTrack = True
+addTriggerMatchingMuon(process,postfix)
+
+from Bianchi.Utilities.electrons import *
+addCutBasedID(process)
+addPFElectronIsolation(process,process.patElectrons)
+addPFElectron(process,postfix)
+getattr(process,"patElectrons").embedTrack = True
+getattr(process,"patElectrons"+postfix).embedTrack = True
+getattr(process,"patElectrons").embedGsfTrack = True
+getattr(process,"patElectrons"+postfix).embedGsfTrack = True
+addTriggerMatchingElectron(process)
+addTriggerMatchingElectron(process,postfix)
+
+if hasattr(process,"patTrigger"):
+    process.patTrigger.processName = '*'
+
+# SC sequence
+process.mergedSuperClusters = cms.EDProducer(
+    "SuperClusterMerger",
+    src = cms.VInputTag(cms.InputTag("correctedHybridSuperClusters"),
+                        cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"))
+    )
+process.selectedSuperClusters = cms.EDFilter(
+    "SuperClusterSelector",
+    src = cms.InputTag("mergedSuperClusters"),
+    cut = cms.string("energy()*sin(position().theta()) > 4"),
+    filter = cms.bool(False)
+    )
+process.makeSCs = cms.Sequence(process.mergedSuperClusters*process.selectedSuperClusters)
+
+    
+#########
+## PAT
+
+process.selectedPatMuonsTriggerMatchUserEmbedded = cms.EDProducer(
+    "MuonsUserEmbedded",
+    muonTag = cms.InputTag("selectedPatMuonsTriggerMatch"),
+    vertexTag = cms.InputTag("offlinePrimaryVertices")
+    )
+
+process.looseMuons = cms.EDFilter(
+    "PATMuonSelector",
+    src = cms.InputTag("selectedPatMuonsTriggerMatchUserEmbedded"),
+    cut = cms.string("pt>15 && (eta<2.4&&eta>-2.4) && isGlobalMuon && globalTrack.isNonnull"),
+    filter = cms.bool(False)
+    )
+
+process.muonLeg = cms.EDFilter(
+    "PATMuonSelector",
+    src = cms.InputTag("selectedPatMuonsTriggerMatchUserEmbedded"),
+    cut = cms.string("pt>15 && (eta<2.1&&eta>-2.1) && isTrackerMuon && numberOfMatches>=2 && globalTrack.isNonnull && globalTrack.hitPattern.numberOfValidMuonHits>=1 && globalTrack.hitPattern.numberOfValidPixelHits>=1 && globalTrack.normalizedChi2<=10 && userFloat('dxyWrtPV')<0.2 && userFloat('PFRelIso04')<0.1 && (triggerObjectMatchesByPath('HLT_Mu11').size()!=0 || triggerObjectMatchesByPath('HLT_Mu9').size()!=0 || triggerObjectMatchesByPath('HLT_Mu15').size()!=0 || triggerObjectMatchesByPath('HLT_Mu15_v1').size()!=0)"),
+    filter = cms.bool(False)
+    )
+
+process.tauLeg = cms.EDFilter(
+    "PATTauSelector",
+    src = cms.InputTag("selectedPatTaus"),
+    cut = cms.string("pt>20 && (eta<2.3&&eta>-2.3) && tauID('leadingTrackFinding')>0.5 && tauID('byLooseIsolation')>0.5 && tauID('againstMuon') && leadPFChargedHadrCand.mva_e_pi < 0.6 && tauID('againstElectronCrackRem')>0.5 "),
+    filter = cms.bool(False)
+    )
+
+
+getattr(process,"selectedPatElectronsTriggerMatch"+postfix).cut = cms.string("(eta<2.4&&eta>-2.4) && !isEBEEGap && et>15 && electronID('simpleEleId95relIso')>6.5")
+getattr(process,"selectedPatElectronsTriggerMatch").cut = cms.string("(eta<2.4&&eta>-2.4) && !isEBEEGap && et>15 && electronID('simpleEleId95relIso')>6.5")
+
+process.secondLeptonVeto = cms.EDFilter(
+    "CandViewCountFilter",
+    src = cms.InputTag("looseMuons"),
+    minNumber = cms.uint32(1),
+    maxNumber = cms.uint32(1),
+    )
+process.noElectronVeto = cms.EDFilter(
+    "CandViewCountFilter",
+    src = cms.InputTag("selectedPatElectronsTriggerMatch"),
+    minNumber = cms.uint32(0),
+    maxNumber = cms.uint32(0),
+    )
+
+
+process.load("Bianchi.Utilities.diTausReconstruction_cff")
+process.diTau = process.muTauPairs.clone()
+process.diTau.srcLeg1 = cms.InputTag("muonLeg")
+process.diTau.srcLeg2 = cms.InputTag("tauLeg")
+if not runOnMC:
+    process.diTau.srcGenParticles = ""
+
+
+process.selectedDiTau = cms.EDFilter(
+    "MuTauPairSelector",
+    src = cms.InputTag("diTau"),
+    cut = cms.string("charge==0 && mt1MET<40")
+    )
+
+process.atLeast1selectedDiTau = cms.EDFilter(
+    "CandViewCountFilter",
+    src = cms.InputTag("selectedDiTau"),
+    minNumber = cms.uint32(1),
+    maxNumber = cms.uint32(999),
+    )
+
+getattr(process,"selectedPatJets").cut = cms.string('pt>5 && abs(eta)<4.5')
+
+process.deltaRJetMuons = cms.EDProducer(
+    "DeltaRNearestMuonComputer",
+    probes = cms.InputTag("selectedPatJets"),
+    objects = cms.InputTag("muonLeg"),
+    )
+process.selectedPatJetsNoMuons = cms.EDProducer(
+    "JetsCleaner",
+    jets =  cms.InputTag("selectedPatJets"),
+    valueMap = cms.InputTag("deltaRJetMuons"),
+    minDeltaR = cms.double(0.3)
+    )
+
+process.deltaRJetTaus = cms.EDProducer(
+    "DeltaRNearestTauComputer",
+    probes = cms.InputTag("selectedPatJetsNoMuons"),
+    objects = cms.InputTag("tauLeg"),
+    )
+process.selectedPatJetsNoMuonsNoTaus = cms.EDProducer(
+    "JetsCleaner",
+    jets =  cms.InputTag("selectedPatJetsNoMuons"),
+    valueMap = cms.InputTag("deltaRJetTaus"),
+    minDeltaR = cms.double(0.3)
+    )
+
+process.muTauStreamAnalyzer = cms.EDAnalyzer(
+    "MuTauStreamAnalyzer",
+    diTaus =  cms.InputTag("selectedDiTau"),
+    jets =  cms.InputTag("selectedPatJetsNoMuonsNoTaus"),
+    isMC = cms.bool(runOnMC),
+    minCorrPt = cms.untracked.double(5.),
+    minJetID  = cms.untracked.double(0.5), # 1=loose,2=medium,3=tight
+    verbose =  cms.untracked.bool(True),
+    )
+
+process.pat = cms.Sequence(
+    process.allEventsFilter+
+    process.primaryVertexFilter+
+    process.scrapping +
+    process.makeSCs +
+    process.fjSequence*
+    process.patDefaultSequence*
+    process.selectedPatMuonsTriggerMatchUserEmbedded*
+    process.looseMuons*
+    process.secondLeptonVeto*
+    process.noElectronVeto*
+    (process.muonLeg+process.tauLeg)*
+    (process.diTau*process.selectedDiTau)*
+    process.atLeast1selectedDiTau+
+    (process.deltaRJetMuons*process.selectedPatJetsNoMuons)*
+    (process.deltaRJetTaus*process.selectedPatJetsNoMuonsNoTaus)*
+    process.muTauStreamAnalyzer+
+    process.printTree1
+    )
+
+if not runOnMC:
+    process.pat.remove(process.printTree1)
+
+process.p = cms.Path(process.pat)
+
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
+process.out.outputCommands = cms.untracked.vstring('drop *',
+                                                   *patEventContentNoCleaning ) 
+process.out.outputCommands.extend( cms.vstring(
+    'keep *_TriggerResults_*_*',
+    'keep *_hltTriggerSummaryAOD_*_*',
+    'keep recoGenParticles_genParticles*_*_*',
+    'keep *_generalTracks_*_*',
+    'keep *_electronGsfTracks_*_*',
+    'keep recoTrackExtras_*_*_*',
+    'keep recoGsfTrackExtras_*_*_*',
+    #'keep TrackingRecHitsOwned_*_*_*',
+    'keep *_selectedSuperClusters_*_*',
+    'keep *_offlineBeamSpot_*_*',
+    'keep *_offlinePrimaryVertices*_*_*',
+    'keep *_particleFlow_*_*',
+    'keep *_selectedPatJetsNoMuonsNoTaus_*_*',
+    'keep *_selectedDiTau_*_*',
+    )
+                                   )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("treeMuTauStream.root")
+                                   )
+
+process.out.SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('p')
+    )
+
+process.out.fileName = cms.untracked.string('patTuples_'+sample+'.root')
+
+#process.outpath = cms.EndPath(process.out)
+process.outpath = cms.EndPath()
+
