b0fd31a78af4e747fe19999896d7367ca815ee4b
inaranjo <>
2012-10-16 08:40:20 +0000

    multicrabs for DATA and SYSU MC from Ivo

--- TauTauStudies/test/prod/publishAll.py
+++ TauTauStudies/test/prod/publishAll.py
@@ -53,7 +53,7 @@ def publishSkim( stream, tasks ):
                 f.write('#skim efficiency: '+str(SKIM)+'/'+str(READ)+' = '+str(SKIM/READ)+'\n')
                 f.write('CMSSW.datasetpath='+datasetpath+'\n')
                 f.write('CMSSW.total_number_of_events= -1\n')
-                f.write('CMSSW.events_per_job = 5000\n')
+                f.write('CMSSW.events_per_job = 2000\n')
                 f.write('\n')
     f.close()
 
@@ -64,38 +64,111 @@ def publishSkim( stream, tasks ):
 
 
 tasksMuTauSummer12 = [
-    #'Run2012A-MuTau-PromptReco-v1-p1-v2_skim'
-    #'Run2012A-MuTau-PromptReco-v1-p2_skim',
-    #'DYJets-MuTau-madgraph-Tarball-v2_skim'
-    #'DYJets-MuTau-madgraph-50_skim',
-    'TTJets-MuTau-madgraph-50_skim',
-    #'WMuNu-MuTau-pythia-50_skim',
-    #'WTauNu-MuTau-pythia-50_skim',
-    #'WENu-MuTau-pythia-50_skim',
-    #'WZ-MuTau-50_skim',
-    #'WW-MuTau-50_skim',
-    #'ZZ-MuTau-50_skim',
-    #'VBFH120-MuTau-pythia-50_skim'
+
     ]
 
 tasksElecTauSummer12 = [
-    #'Run2012A-ElecTau-PromptReco-v1-p1-v2_skim'
-    #'Run2012A-ElecTau-PromptReco-v1-p2_skim',
-    #'DYJets-ElecTau-madgraph-Tarball-v2_skim'
-    #'DYJets-ElecTau-madgraph-50_skim',
-    'TTJets-ElecTau-madgraph-50_skim',
-    #'WENu-ElecTau-pythia-50_skim',
-    #'WTauNu-ElecTau-pythia-50_skim',
-    #'WMuNu-ElecTau-pythia-50_skim',
-    #'WW-ElecTau-50_skim',
-    #'WZ-ElecTau-50_skim',
-    #'ZZ-ElecTau-50_skim',
-    #'VBFH120-ElecTau-pythia-50_skim',
+##     #'Run2012A-ElecTau-ReReco13Jul2012-v1-iter3_skim', 
+## #'Run2012B-ElecTau-ReReco13Jul2012-v1-iter3_skim', 
+'Run2012C-ElecTau-ReReco24Aug2012-v1-iter3_skim', 
+## #'Run2012C-ElecTau-PromptReco-v2-iter3_skim',
+## #'Run2012C-ElecTau-PromptReco-v2p2-198934-203002_skim',
+
+############################
+#####Embedded ETau
+##########################
+
+##     #'Run2012A-ElecTau-53X-ReReco13Jul2012-v1-Embedded-EleJet-iter3_skim', 
+##     #'Run2012B-ElecTau-53X-ReReco13Jul2012-v1-Embedded-EleJet-iter3_skim', 
+##     #'Run2012C-ElecTau-53X-PromptReco-v2-Embedded-EleJet-iter3_skim',
+##     #'Run2012C-ElecTau-53X-ReReco24Aug2012-Embedded-EleJet-iter3_skim',
+##     #'Run2012C-ElecTau-53X-PromptReco-v2p2-198934-203002-Embedded-EleJet_skim',
+
+
+#global dbs
+## #'Run2012A-ElecTau-53X-ReReco13Jul2012-v1-Embedded-EleJet-iter4_skim', 
+## #'Run2012A-ElecTau-53X-recover_06Aug2012-Embedded-EleJet-iter4_skim', 
+## 'Run2012B-ElecTau-53X-ReReco13Jul2012-v1-Embedded-EleJet-iter5_skim', 
+## #'Run2012C-ElecTau-53X-ReReco24Aug2012-Embedded-EleJet-iter4_skim', 
+## #'Run2012C-ElecTau-53X-PromptReco-v2-Embedded-EleJet-iter4_skim',
+
+
+## ##52X
+
+## #'Run2012A-ElecTau-PromptReco-v1-Embedded-EleJet_skim',
+##     #'Run2012B-ElecTau-PromptReco-v1-p1-Embedded-EleJet_skim',   
+## #'Run2012B-ElecTau-PromptReco-v1-p2-Embedded-EleJet_skim', 
+##     #'Run2012B-ElecTau-PromptReco-v1-p3-Embedded-EleJet_skim',
+
+
+    ###Not to publish
+    ###'Run2012C-ElecTau-ReReco24Aug2012-Embedded-EleJet_skim', 
+
+
+#################################################################
+###################MC
+#################################################################
+
+
+#########################################################################
+##Signal
+
+##     #'SUSYGGH80-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH90-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH100-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH110-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH120-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH130-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH140-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH160-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH180-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH200-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH250-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH300-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH350-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH400-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH450-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH500-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH600-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH700-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH800-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH900-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYGGH1000-ElecTau-pythia-tauola-iter1_skim',
+
+###
+
+##     #'SUSYBBH80-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH90-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH100-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH110-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH120-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH130-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH140-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH160-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH180-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH200-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH250-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH350-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH400-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH450-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH500-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH600-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH700-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH800-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH900-ElecTau-pythia-tauola-iter1_skim',
+##     #'SUSYBBH1000-ElecTau-pythia-tauola-iter1_skim',
+
+#########################################################################
+##Embedded MC
+#########################################################################
+## #'DYJets-ElecTau-8TeV-50-madgraph-PUS7-Embedded_skim'
+
+
     ]
 
 
 
 
 #publishSkim( "MuTauStream_30Mar2012_patch50X_v4",  tasksMuTauSummer12)
-publishSkim( "ElecTauStream_30Mar2012_patch50X_v4",tasksElecTauSummer12)
+publishSkim( "ElecTauStream_07Oct2012-part6",tasksElecTauSummer12)
 
