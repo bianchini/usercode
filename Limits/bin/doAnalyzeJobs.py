9384feb3479954fe8162558db104f69acf60b7a8
ndaci <>
2012-10-31 17:00:52 +0000

    Put analysis as last argument (no last argument=>Nominal).

--- Limits/bin/doAnalyzeJobs.py
+++ Limits/bin/doAnalyzeJobs.py
@@ -29,17 +29,18 @@ def analyze(mH,category,analysis,variable,xtitle,unity,outputDir,magnify):
     f.write('eval `scram runtime -sh`\n')
     f.write('source /opt/exp_soft/cms/cms_ui_env_crab.sh\n')
     ##
-    f.write('analyze'+stream+'_HCP '+str(mH)+' '+category+' '+analysis+' '+variable+' '+xtitle+' '+unity +' '+outputDir +' '+str(magnify)+' > '+fileLog+'\n')
+    f.write('analyze'+stream+'_HCP '+str(mH)+' '+category+' '+variable+' '+xtitle+' '+unity +' '+outputDir +' '+str(magnify)+' '+analysis+' > '+fileLog+'\n')
     f.close()
-    os.system('chmod u+x batch/*.sh')
+    os.system('chmod u+x batch/analyze/job*.sh')
   
 ###########################################
 ###########################################
 
-outputDir = 'test2'
+outputDir = 'test3'
 
 mH = [125]
 category = ['novbf','vbf','boost','inclusive']
+#category = ['vbf']
 mode     = ['','Low','High']
 analysis = ['','TauUp','TauDown','JetUp','JetDown']
 variable = ['diTauNSVfitMass']
