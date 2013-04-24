99ed265aa299ad3eaf763c0c4bd64e187137d29d
bianchi <>
2013-04-24 09:29:56 +0000

    *** empty log message ***

--- TTHStudies/bin/TransferFunctions_SL.cc
+++ TTHStudies/bin/TransferFunctions_SL.cc
@@ -12,6 +12,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1D.h"
+#include "TH2F.h"
 #include "TF1.h"
 #include "TF2.h"
 #include "TGraph.h"
@@ -119,14 +120,54 @@ int main(int argc, const char* argv[])
   TH1F* accHeavyBin0  = new TH1F("accHeavyBin0","",100,0,400);
   TH1F* accHeavyBin1  = new TH1F("accHeavyBin1","",100,0,400);
 
+  TH1F* resolLightBin0  = new TH1F("resolLightBin0","",20,0,200);
+  TH1F* respLightBin0   = new TH1F("respLightBin0","", 20,0,200);
+  TH1F* resolLightBin1  = new TH1F("resolLightBin1","",20,0,200);
+  TH1F* respLightBin1   = new TH1F("respLightBin1","", 20,0,200);
+
+  TH1F* resolHeavyBin0  = new TH1F("resolHeavyBin0","",20,0,200);
+  TH1F* respHeavyBin0   = new TH1F("respHeavyBin0","", 20,0,200);
+  TH1F* resolHeavyBin1  = new TH1F("resolHeavyBin1","",20,0,200);
+  TH1F* respHeavyBin1   = new TH1F("respHeavyBin1","", 20,0,200);
+
   for(int k = 0; k < 2; k++){
 
     TH1F* accPass   = (TH1F*)accLightBin0->Clone(Form("accPass%d",k)); //accPass->Sumw2();
     TH1F* accTot    = (TH1F*)accLightBin0->Clone(Form("accTot%d",k));  //accTot->Sumw2();
+
+    TH2F* hCorrLight = new TH2F("hCorrLight","", 20, 0,200, 40, 0,200);
+    treeJetsLight->Draw("ptReco:pt>>hCorrLight", Form("(ptReco>30)*(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",etaBinning[k],etaBinning[k+1]) );
+
     treeJetsLight->Draw(Form("pt>>accTot%d",k),  Form("(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",            etaBinning[k],etaBinning[k+1]) );
     treeJetsLight->Draw(Form("pt>>accPass%d",k), Form("(ptReco>30)*(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",etaBinning[k],etaBinning[k+1]) );
     accPass->Sumw2();
     accTot->Sumw2();
+
+    for(int j = 1; j<=hCorrLight->GetNbinsX(); j++){
+
+      TH1F* hSlice = (TH1F*)hCorrLight->ProjectionY("_py",j,j);
+      if(hSlice->GetEntries()<8) continue;
+      cout << hSlice->GetMean() << ", " << hSlice->GetRMS() << endl;
+
+      TF1* resol = new TF1("resol","gaus", 30,300);
+      hSlice->Fit(resol);
+
+      if(k==0){
+	resolLightBin0->SetBinContent(j, resol->GetParameter(2));
+	resolLightBin0->SetBinError  (j, resol->GetParameter(2)/TMath::Sqrt(hSlice->GetEntries()));
+	respLightBin0->SetBinContent (j, resol->GetParameter(1));
+	respLightBin0->SetBinError   (j, resol->GetParameter(1)/TMath::Sqrt(hSlice->GetEntries()));
+      }
+      if(k==1){
+	resolLightBin1->SetBinContent(j, resol->GetParameter(2));
+	resolLightBin1->SetBinError  (j, resol->GetParameter(2)/TMath::Sqrt(hSlice->GetEntries()));
+	respLightBin1->SetBinContent (j, resol->GetParameter(1));
+	respLightBin1->SetBinError   (j, resol->GetParameter(1)/TMath::Sqrt(hSlice->GetEntries()));
+      }
+
+      delete resol;
+    }
+
 
     if(k==0)
       accLightBin0->Divide( accPass,accTot, 1.0,1.0);
@@ -136,16 +177,46 @@ int main(int argc, const char* argv[])
     accPass->Reset();
     accTot->Reset();
 
+    TH2F* hCorrHeavy = new TH2F("hCorrHeavy","", 20, 0,200, 40, 0,200);
+    treeJetsHeavy->Draw("ptReco:pt>>hCorrHeavy", Form("(ptReco>30)*(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",etaBinning[k],etaBinning[k+1]) );
+
     treeJetsHeavy->Draw(Form("pt>>accTot%d",k),  Form("(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",            etaBinning[k],etaBinning[k+1]) );
     treeJetsHeavy->Draw(Form("pt>>accPass%d",k), Form("(ptReco>30)*(TMath::Abs(eta)>%f && TMath::Abs(eta)<%f)",etaBinning[k],etaBinning[k+1]) );
     accPass->Sumw2();
     accTot->Sumw2();
+
+    for(int j = 1; j<=hCorrHeavy->GetNbinsX(); j++){
+
+      TH1F* hSlice = (TH1F*)hCorrHeavy ->ProjectionY("_py",j,j);
+      if(hSlice->GetEntries()<8) continue;
+      cout << hSlice->GetMean() << ", " << hSlice->GetRMS() << endl;
+
+      TF1* resol = new TF1("resol","gaus", 30,300);
+      hSlice->Fit(resol);
+
+      if(k==0){
+	resolHeavyBin0->SetBinContent(j, resol->GetParameter(2));
+	resolHeavyBin0->SetBinError  (j, resol->GetParameter(2)/TMath::Sqrt(hSlice->GetEntries()));
+	respHeavyBin0->SetBinContent (j, resol->GetParameter(1));
+	respHeavyBin0->SetBinError   (j, resol->GetParameter(1)/TMath::Sqrt(hSlice->GetEntries()));
+      }
+      if(k==1){
+	resolHeavyBin1->SetBinContent(j, resol->GetParameter(2));
+	resolHeavyBin1->SetBinError  (j, resol->GetParameter(2)/TMath::Sqrt(hSlice->GetEntries()));
+	respHeavyBin1->SetBinContent (j, resol->GetParameter(1));
+	respHeavyBin1->SetBinError   (j, resol->GetParameter(1)/TMath::Sqrt(hSlice->GetEntries()));
+      }
+
+      delete resol;
+    }
+
 
     if(k==0)
       accHeavyBin0->Divide( accPass,accTot, 1.0,1.0);
     if(k==1)
       accHeavyBin1->Divide( accPass,accTot, 1.0,1.0);
       
+    delete hCorrLight;  delete hCorrHeavy;
   }
 
   TF1* turnOnLightBin0 = new TF1("turnOnBin0","TMath::Erf([0]*x+[1])+[2]", 0,400);
@@ -157,29 +228,101 @@ int main(int argc, const char* argv[])
   accHeavyBin0->Fit(turnOnHeavyBin0);  
   accHeavyBin1->Fit(turnOnHeavyBin1);
 
+  TF1* sigmaLightBin0 = new TF1("sigmaLightBin0","x*sqrt([0]*[0]/x + [1]*[1]/x/x)",40,200);
+  TF1* sigmaLightBin1 = new TF1("sigmaLightBin1","x*sqrt([0]*[0]/x + [1]*[1]/x/x)",40,200);
+  TF1* sigmaHeavyBin0 = new TF1("sigmaHeavyBin0","x*sqrt([0]*[0]/x + [1]*[1]/x/x)",40,200);
+  TF1* sigmaHeavyBin1 = new TF1("sigmaHeavyBin1","x*sqrt([0]*[0]/x + [1]*[1]/x/x)",40,200);
+  TF1* meanLightBin0  = new TF1("meanLightBin0", "x*[0] + [1]",40,200);
+  TF1* meanLightBin1  = new TF1("meanLightBin1", "x*[0] + [1]",40,200);
+  TF1* meanHeavyBin0  = new TF1("meanHeavyBin0", "x*[0] + [1]",40,200);
+  TF1* meanHeavyBin1  = new TF1("meanHeavyBin1", "x*[0] + [1]",40,200);
 
-
-
-  fout = new TFile("ControlPlots.root","UPDATE");
-  fout->cd();
-
-  accLightBin0->Write();
-  accLightBin1->Write();
-  accHeavyBin0->Write();
-  accHeavyBin1->Write();
-
-  fout->Close();
-  delete fout;
-
-  return 0;
-  
-
-
+  resolLightBin0->Fit(sigmaLightBin0,"","",40,160);
+  resolLightBin1->Fit(sigmaLightBin1,"","",40,160);
+  resolHeavyBin0->Fit(sigmaHeavyBin0,"","",40,160);
+  resolHeavyBin1->Fit(sigmaHeavyBin1,"","",40,160);
+  respLightBin0->Fit (meanLightBin0, "","",40,160);
+  respLightBin1->Fit (meanLightBin1, "","",40,160);
+  respHeavyBin0->Fit (meanHeavyBin0, "","",40,160);
+  respHeavyBin1->Fit (meanHeavyBin1, "","",40,160);
 
 
   RooWorkspace *w = new RooWorkspace("transferFuntions","all functions for SL ttH");
 
-  
+  RooRealVar param0AccLightBin0("param0AccLightBin0","", turnOnLightBin0->GetParameter(0));
+  RooRealVar param1AccLightBin0("param1AccLightBin0","", turnOnLightBin0->GetParameter(1));
+  RooRealVar param2AccLightBin0("param2AccLightBin0","", turnOnLightBin0->GetParameter(2));
+  RooRealVar param0AccLightBin1("param0AccLightBin1","", turnOnLightBin1->GetParameter(0));
+  RooRealVar param1AccLightBin1("param1AccLightBin1","", turnOnLightBin1->GetParameter(1));
+  RooRealVar param2AccLightBin1("param2AccLightBin1","", turnOnLightBin1->GetParameter(2));
+  RooRealVar param0AccHeavyBin0("param0AccHeavyBin0","", turnOnHeavyBin0->GetParameter(0));
+  RooRealVar param1AccHeavyBin0("param1AccHeavyBin0","", turnOnHeavyBin0->GetParameter(1));
+  RooRealVar param2AccHeavyBin0("param2AccHeavyBin0","", turnOnHeavyBin0->GetParameter(2));
+  RooRealVar param0AccHeavyBin1("param0AccHeavyBin1","", turnOnHeavyBin1->GetParameter(0));
+  RooRealVar param1AccHeavyBin1("param1AccHeavyBin1","", turnOnHeavyBin1->GetParameter(1));
+  RooRealVar param2AccHeavyBin1("param2AccHeavyBin1","", turnOnHeavyBin1->GetParameter(2));
+
+  RooRealVar param0resolLightBin0("param0resolLightBin0","", sigmaLightBin0->GetParameter(0));
+  RooRealVar param1resolLightBin0("param1resolLightBin0","", sigmaLightBin0->GetParameter(1));
+  RooRealVar param0resolLightBin1("param0resolLightBin1","", sigmaLightBin1->GetParameter(0));
+  RooRealVar param1resolLightBin1("param1resolLightBin1","", sigmaLightBin1->GetParameter(1));
+  RooRealVar param0respLightBin0 ("param0respLightBin0","",  meanLightBin0->GetParameter(0));
+  RooRealVar param1respLightBin0 ("param1respLightBin0","",  meanLightBin0->GetParameter(1));
+  RooRealVar param0respLightBin1 ("param0respLightBin1","",  meanLightBin1->GetParameter(0));
+  RooRealVar param1respLightBin1 ("param1respLightBin1","",  meanLightBin1->GetParameter(1));
+
+  RooRealVar param0resolHeavyBin0("param0resolHeavyBin0","", sigmaHeavyBin0->GetParameter(0));
+  RooRealVar param1resolHeavyBin0("param1resolHeavyBin0","", sigmaHeavyBin0->GetParameter(1));
+  RooRealVar param0resolHeavyBin1("param0resolHeavyBin1","", sigmaHeavyBin1->GetParameter(0));
+  RooRealVar param1resolHeavyBin1("param1resolHeavyBin1","", sigmaHeavyBin1->GetParameter(1));
+  RooRealVar param0respHeavyBin0 ("param0respHeavyBin0","",  meanHeavyBin0->GetParameter(0));
+  RooRealVar param1respHeavyBin0 ("param1respHeavyBin0","",  meanHeavyBin0->GetParameter(1));
+  RooRealVar param0respHeavyBin1 ("param0respHeavyBin1","",  meanHeavyBin1->GetParameter(0));
+  RooRealVar param1respHeavyBin1 ("param1respHeavyBin1","",  meanHeavyBin1->GetParameter(1));
+
+
+  w->import( param0AccLightBin0 ) ;
+  w->import( param1AccLightBin0 ) ;
+  w->import( param2AccLightBin0 ) ;
+  w->import( param0AccLightBin1 ) ;
+  w->import( param1AccLightBin1 ) ;
+  w->import( param2AccLightBin1 ) ;
+  w->import( param0AccHeavyBin0 ) ;
+  w->import( param1AccHeavyBin0 ) ;
+  w->import( param2AccHeavyBin0 ) ;
+  w->import( param0AccHeavyBin1 ) ;
+  w->import( param1AccHeavyBin1 ) ;
+  w->import( param2AccHeavyBin1 ) ;
+
+  w->import(  param0resolLightBin0 );
+  w->import(  param1resolLightBin0 );
+  w->import(  param0resolLightBin1 );
+  w->import(  param1resolLightBin1 );
+  w->import(  param0respLightBin0 );
+  w->import(  param1respLightBin0 );
+  w->import(  param0respLightBin1 );
+  w->import(  param1respLightBin1 );
+  w->import(  param0resolHeavyBin0 );
+  w->import(  param1resolHeavyBin0 );
+  w->import(  param0resolHeavyBin1 );
+  w->import(  param1resolHeavyBin1 );
+  w->import(  param0respHeavyBin0 );
+  w->import(  param1respHeavyBin0 );
+  w->import(  param0respHeavyBin1 );
+  w->import(  param1respHeavyBin1 );
+
+
+  RooRealVar csvRecoLight("csvReco","csv", -0.1, 1.1);
+  RooDataSet datasetBtagLight("datasetBtagLight","dataset", RooArgSet(csvRecoLight), Import( *treeJetsLight ) );
+  RooKeysPdf pdfCsvLight("pdfCsvLight","", csvRecoLight, datasetBtagLight);
+  w->import( pdfCsvLight );
+
+  RooRealVar csvRecoHeavy("csvReco","csv", -0.1, 1.1);
+  RooDataSet datasetBtagHeavy("datasetBtagHeavy","dataset", RooArgSet(csvRecoHeavy), Import( *treeJetsHeavy ) );
+  RooKeysPdf pdfCsvHeavy("pdfCsvHeavy","", csvRecoHeavy, datasetBtagHeavy);
+  w->import( pdfCsvHeavy );
+
+
 
   RooRealVar v1       ("X1","sqrt(X1*X2)", 0.0, 0.5);
   RooRealVar v2       ("X2","X1-X2",      -0.6, 0.6);
@@ -364,6 +507,16 @@ int main(int argc, const char* argv[])
 
   w->writeToFile(outFileName.c_str(),kTRUE);
 
+  TCanvas *c1CsvLight = new TCanvas("c1CsvLight","canvas",10,30,650,600);
+  RooPlot* plotCsvLight = csvRecoLight.frame(Bins(20),Title("cvs light"));
+  datasetBtagLight.plotOn(plotCsvLight);
+  pdfCsvLight.plotOn(plotCsvLight);
+
+  TCanvas *c1CsvHeavy = new TCanvas("c1CsvHeavy","canvas",10,30,650,600);
+  RooPlot* plotCsvHeavy = csvRecoHeavy.frame(Bins(20),Title("cvs heavy"));
+  datasetBtagHeavy.plotOn(plotCsvHeavy);
+  pdfCsvHeavy.plotOn(plotCsvHeavy);
+
   TCanvas *c1V1 = new TCanvas("c1V1","canvas",10,30,650,600);
   RooPlot* plotV1 = v1.frame(Bins(20),Title("v1"));
   histV1.plotOn(plotV1);
@@ -404,6 +557,14 @@ int main(int argc, const char* argv[])
   fout = new TFile("ControlPlots.root","UPDATE");
   fout->cd();
 
+  c1CsvLight->cd();
+  plotCsvLight->Draw();
+  c1CsvLight->Write("",TObject::kOverwrite);
+
+  c1CsvHeavy->cd();
+  plotCsvHeavy->Draw();
+  c1CsvHeavy->Write("",TObject::kOverwrite);
+
   c1V1->cd();
   plotV1->Draw();
   c1V1->Write("",TObject::kOverwrite);
@@ -436,10 +597,18 @@ int main(int argc, const char* argv[])
   plotGammaW->Draw();
   c1GammaW->Write("",TObject::kOverwrite);
 
-  accLightBin0->Write();
-  accLightBin1->Write();
-  accHeavyBin0->Write();
-  accHeavyBin1->Write();
+  accLightBin0->Write("",TObject::kOverwrite);
+  accLightBin1->Write("",TObject::kOverwrite);
+  accHeavyBin0->Write("",TObject::kOverwrite);
+  accHeavyBin1->Write("",TObject::kOverwrite);
+  resolLightBin0->Write("",TObject::kOverwrite);
+  respLightBin0->Write("",TObject::kOverwrite);
+  resolLightBin1->Write("",TObject::kOverwrite);
+  respLightBin1->Write("",TObject::kOverwrite);
+  resolHeavyBin0->Write("",TObject::kOverwrite);
+  respHeavyBin0->Write("",TObject::kOverwrite);
+  resolHeavyBin1->Write("",TObject::kOverwrite);
+  respHeavyBin1->Write("",TObject::kOverwrite);
 
   fout->Close();
   delete fout;
